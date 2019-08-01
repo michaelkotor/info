01ef56e3ebbe40c043152ed2367e9d901f44a62a
Michael Kotor <milan3020@gmail.com>
2019-08-01 15:42:19 +0300

    @RestController -> @Controller

--- src/main/java/com/kotor/controllers/MainController.java
+++ src/main/java/com/kotor/controllers/MainController.java
@@ -2,12 +2,13 @@ package com.kotor.controllers;
 
 import com.kotor.public_api.Theatre;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 public class MainController {
     private final Theatre theatre;
 
